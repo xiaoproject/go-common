3d40e9a31dbf47f93a58b8832c572e2451ab297f
xiao <626223818@qq.com>
2020-05-13 17:09:59 +0800

    Update Data.go
    string to int

--- function/Data.go
+++ function/Data.go
@@ -3,6 +3,7 @@ package function
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"strconv"
 )
 
 /**
@@ -25,4 +26,16 @@ func FunMd5(str string) string {
 	h.Write([]byte(str))
 	return hex.EncodeToString(h.Sum(nil))
 }
+
+// string to int
+// @param source 字符串
+func Str2Int(source string) int {
+
+	i, e := strconv.Atoi(source)
+	if e != nil{
+		return 0
+	}
+	return i
+}
+
 
